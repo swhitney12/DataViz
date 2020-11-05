dbd59f1e6babe7d015b99d9ba9ace47fb5bf0ae3
Sabrina <swhitney@trinity.edu>
2020-11-05 17:34:43 -0600

    Update interactiveline.js

--- HW5_D3/interactiveline.js
+++ HW5_D3/interactiveline.js
@@ -11,7 +11,7 @@ var y = d3.scaleLinear().range([height, 0]);
 //setting up axes
 var xAxis = g => g  
     .attr("transform", `translate(0,${height - margin.bottom})`)
-    .call(d3.axisBottom(x).ticks(10).tickFormat(parseDate));
+    .call(d3.axisBottom(x).ticks(10).tickFormat(d3.timeParse("%Y-%m-%d")));
 
 var yAxis = g => g
      .attr("transform", `translate(${margin.left},0)`)
