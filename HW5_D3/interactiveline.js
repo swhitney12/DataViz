06f4edb9a80b858442f2a8f256775b4c1c7d924d
Sabrina <swhitney@trinity.edu>
2020-11-04 19:47:19 -0600

    Update interactiveline.js

--- HW5_D3/interactiveline.js
+++ HW5_D3/interactiveline.js
@@ -6,19 +6,22 @@ xAxis = g => g
     .attr("transform", `translate(0,${height - margin.bottom})`)
     .call(d3.axisBottom(x));
 
-yAxis = g => g
-    .attr("transform", `translate(0,${width - margin.right})`)
-    .call(d3.axisLeft(y));
+// yAxis = g => g
+//     .attr("transform", `translate(0,${width - margin.right})`)
+//     .call(d3.axisLeft(y));
 
 
 x = d3.scaleLinear([0,10], [margin.left, width - margin.right]);
-y = d3.scaleLinear([0,10], [margin.bottom, height - margin.top])
+// y = d3.scaleLinear([0,10], [margin.bottom, height - margin.top])
 
 const svg = d3.select("body")
     .append("svg")
     .attr("width", width)
     .attr("height", height)
     .attr("viewBox", [0,0,width,height]);
+
+const brush = d3.brushX()
+    .extent([[margin.left,margin.top],[width-margin.right,height-margin.bottom]])
 
 svg.append("g").call(xAxis);
 svg.append("g").call(yAxis);
