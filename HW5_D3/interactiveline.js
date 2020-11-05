47f052a28372e46c05894feefa351247139b428e
Sabrina <swhitney@trinity.edu>
2020-11-05 00:00:45 -0600

    Looking for csvread issue

--- HW5_D3/interactiveline.js
+++ HW5_D3/interactiveline.js
@@ -2,11 +2,13 @@ var margin = {top:20, right: 20, bottom:20, left:20},
     height = 300,
     width = 1000;
 
-var parseDate = d3.timeFormat("%Y-%m-%e").parse;
+//function for date parsing
+//var parseDate = d3.timeFormat("%Y-%m-%e").parse;
 
 //var x = d3.scaleTime().range([0, width]);
 //var y = d3.scaleLinear().range([height, 0]);
 
+//setting up axes
 //var xAxis = g => g  
 //    .attr("transform", `translate(0,${height - margin.bottom})`)
 //    .call(d3.axisBottom(x).ticks(5));
@@ -15,27 +17,38 @@ var parseDate = d3.timeFormat("%Y-%m-%e").parse;
 //     .attr("transform", `translate(${margin.left},0)`)
 //     .call(d3.axisLeft(y).ticks(5));
 
+//creating line
 //var valueline = d3.line()
 //    .x(function(d) { return x(d.date);})
 //    .y(function(d) { return y(d.close);})
 
+//creating svg & viewbox for brushing
 //const svg = d3.select("body")
 //    .append("svg")
 //        .attr("width", width)
 //        .attr("height", height)
 //    .attr("viewBox", [0,0,width,height]);
 
+//creating brush
 //const brush = d3.brushX()
 //    .extent([[margin.left,margin.top],[width-margin.right,height-margin.bottom]])
 
 //getting the data for line1
-d3.csv("XOM-XOM.csv")
-    .then((data) => {
-        console.log(data);
-        data.forEach(function(d)  {
-            d.Date = parseDate(d.Date);
-            d.Close = +d.Close;
-        });
+
+// d3.csv("XOM-XOM.csv")
+//     .then((data) => {
+//         console.log(data);
+//         data.forEach(function(d)  {
+//             d.Date = parseDate(d.Date);
+//             d.Close = +d.Close;
+//         });
+
+d3.csv("XOM-XOM.csv", function(error, data) {
+    console.log(data);
+//    data.forEach(function(d)  {
+//         d.Date = parseDate(d.Date);
+//         d.Close = +d.Close;
+//    });
 
 //    x.domain(d3.extent(data, function(d) { return d.date; }));
 //    y.domain([0, d3.max(data, function(d) { return d.close; })]);
@@ -47,8 +60,5 @@ d3.csv("XOM-XOM.csv")
 //    svg.append("g").call(xAxis);
 //    svg.append("g").call(brush);
 //    svg.append("g").call(yAxis);
-    })
-    //.catch((error) => {
-    //    console.error("error loading data");
-    //});
+});
 
