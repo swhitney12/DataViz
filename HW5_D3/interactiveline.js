5e47ca36626c2580d84a87f8620d6b24ece17f2c
Sabrina <swhitney@trinity.edu>
2020-11-04 22:41:05 -0600

    Update interactiveline.js

--- HW5_D3/interactiveline.js
+++ HW5_D3/interactiveline.js
@@ -30,7 +30,7 @@ const brush = d3.brushX()
 
 //getting the data for line1
 d3.csv("XOM-XOM.csv", function(error, data) {
-    console.log(data[1].Date);
+    console.log(data);
     // data.forEach(function(d)  {
     //     d.date = parseDate(d.Date);
     //     d.close = +d.Close;
