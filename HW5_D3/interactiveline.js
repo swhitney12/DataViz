d493a90b1ec0309584017a7fcd12afd78dab03a9
Sabrina <swhitney@trinity.edu>
2020-11-05 17:02:06 -0600

    re-implement previous code

--- HW5_D3/interactiveline.js
+++ HW5_D3/interactiveline.js
@@ -5,17 +5,17 @@ var margin = {top:20, right: 20, bottom:20, left:20},
 //function for date parsing
 var parseDate = d3.timeParse("%Y-%m-%e");
 
-//var x = d3.scaleTime().range([0, width]);
-//var y = d3.scaleLinear().range([height, 0]);
+var x = d3.scaleTime().range([0, width]);
+var y = d3.scaleLinear().range([height, 0]);
 
 //setting up axes
-//var xAxis = g => g  
-//    .attr("transform", `translate(0,${height - margin.bottom})`)
-//    .call(d3.axisBottom(x).ticks(5));
+var xAxis = g => g  
+    .attr("transform", `translate(0,${height - margin.bottom})`)
+    .call(d3.axisBottom(x).ticks(5));
 
-//var yAxis = g => g
-//     .attr("transform", `translate(${margin.left},0)`)
-//     .call(d3.axisLeft(y).ticks(5));
+var yAxis = g => g
+     .attr("transform", `translate(${margin.left},0)`)
+     .call(d3.axisLeft(y).ticks(5));
 
 //creating line
 //var valueline = d3.line()
@@ -23,18 +23,17 @@ var parseDate = d3.timeParse("%Y-%m-%e");
 //    .y(function(d) { return y(d.close);})
 
 //creating svg & viewbox for brushing
-//const svg = d3.select("body")
-//    .append("svg")
-//        .attr("width", width)
-//        .attr("height", height)
-//    .attr("viewBox", [0,0,width,height]);
+const svg = d3.select("body")
+    .append("svg")
+        .attr("width", width)
+        .attr("height", height)
+    .attr("viewBox", [0,0,width,height]);
 
 //creating brush
-//const brush = d3.brushX()
-//    .extent([[margin.left,margin.top],[width-margin.right,height-margin.bottom]])
+const brush = d3.brushX()
+    .extent([[margin.left,margin.top],[width-margin.right,height-margin.bottom]])
 
 //getting the data for line1
-
 d3.csv("XOM-XOM.csv")
      .then(function(data) {
          console.log(data);
@@ -43,13 +42,6 @@ d3.csv("XOM-XOM.csv")
              d.Close = +d.Close;
          });
 
-//d3.csv("XOM-XOM.csv", function(error, data) {
-//    console.log(data);
-//    data.forEach(function(d)  {
-//         d.Date = parseDate(d.Date);
-//         d.Close = +d.Close;
-//    });
-
 //    x.domain(d3.extent(data, function(d) { return d.date; }));
 //    y.domain([0, d3.max(data, function(d) { return d.close; })]);
 
@@ -57,8 +49,8 @@ d3.csv("XOM-XOM.csv")
 //        .attr("class", "line")
 //        .attr("d", valueline(data));
 
-//    svg.append("g").call(xAxis);
-//    svg.append("g").call(brush);
-//    svg.append("g").call(yAxis);
+    svg.append("g").call(xAxis);
+    svg.append("g").call(brush);
+    svg.append("g").call(yAxis);
 });
 
