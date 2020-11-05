4f7112dcbdd320b20624d0074a7b6150f123309e
Sabrina <swhitney@trinity.edu>
2020-11-04 21:32:25 -0600

    Dataforline

--- HW5_D3/interactiveline.js
+++ HW5_D3/interactiveline.js
@@ -2,6 +2,8 @@ margin = ({top:20, right: 20, bottom:20, left:20});
 height = 300;
 width = 1000;
 
+var parseDate = d3.timeFormat("%d-%b-%y").parse;
+
 xAxis = g => g  
     .attr("transform", `translate(0,${height - margin.bottom})`)
     .call(d3.axisBottom(x));
@@ -10,6 +12,9 @@ yAxis = g => g
      .attr("transform", `translate(${margin.left},0)`)
      .call(d3.axisLeft(y));
 
+var valueline = d3.svg.line()
+    .x(function(d) {return x(d.Date);})
+    .y(function(d) {return y(d.Close);})
 
 x = d3.scaleLinear([0,10], [margin.left, width - margin.right]);
 y = d3.scaleLinear([0,10], [margin.left, height - margin.bottom])
@@ -23,7 +28,23 @@ const svg = d3.select("body")
 const brush = d3.brushX()
     .extent([[margin.left,margin.top],[width-margin.right,height-margin.bottom]])
 
-svg.append("g").call(xAxis);
-svg.append("g")
-    .call(brush);
-svg.append("g").call(yAxis);
+//getting the data for line1
+d3.csv("XOM - XOM.csv", function(error,data) {
+    data.forEach(function(d) {
+        d.date = parseDate(d.Date);
+        d.close = +d.Close;
+    });
+
+    x.domain(d3.extent(data, function(d) { return d.date; }));
+    y.domain([0, d3.max(data, function(d) { return d.close; })]);
+
+    svg.append("path")
+        .attr("class", "line")
+        .attr("d", valueline(data));
+
+    svg.append("g").call(xAxis);
+    svg.append("g")
+        .call(brush);
+    svg.append("g").call(yAxis);
+});
+
