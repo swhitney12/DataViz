57ad8a2b64dbdb80838ed02aa5d765405869b6df
Sabrina <swhitney@trinity.edu>
2020-11-05 23:25:37 -0600

    Update interactiveline.js

--- HW5_D3/interactiveline.js
+++ HW5_D3/interactiveline.js
@@ -55,8 +55,8 @@ var context = svg.append("g")
 //creating brush
 const brush = d3.brushX()
     .extent([[margin2.left,margin2.top - 10],[width-margin2.right,height2-margin2.bottom]])
-    .on("brush", brushed)
-    .on("end", brushended);
+    .on("brush", brushed);
+    //.on("end", brushended);
 
 function brushed({selection}) {
     if(selection) {
