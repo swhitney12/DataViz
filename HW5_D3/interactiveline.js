ff9fa082644a8b33255f95af1357779bc6737125
Sabrina <swhitney@trinity.edu>
2020-11-05 23:02:16 -0600

    Update interactiveline.js

--- HW5_D3/interactiveline.js
+++ HW5_D3/interactiveline.js
@@ -1,8 +1,8 @@
 var margin = {top:10, right: 10, bottom:100, left:40},
-    margin2 = {top:300, right:10, bottom: 5, left:40},
+    margin2 = {top:400, right:10, bottom: 5, left:40},
     width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom,
-    height2 = 660 - margin2.top - margin2.bottom;
+    height2 = 760 - margin2.top - margin2.bottom;
 
 
 //function for date parsing
