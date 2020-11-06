0df111c9070b028b04262d22af63fc75d4173a35
Sabrina <swhitney@trinity.edu>
2020-11-05 23:12:15 -0600

    Update interactiveline.js

--- HW5_D3/interactiveline.js
+++ HW5_D3/interactiveline.js
@@ -51,9 +51,7 @@ var context = svg.append("g")
 
 //creating brush
 const brush = d3.brushX()
-    .x(x2);
-    //.on("brush", brushed);
-     //.extent([[margin2.left,margin2.top],[width-margin2.right,height2-margin2.bottom]])
+    .extent([[margin2.left,margin2.top + 10],[width-margin2.right,height2-margin2.bottom]])
 
 //getting the data for line1
 d3.csv("XOM-XOM.csv")
