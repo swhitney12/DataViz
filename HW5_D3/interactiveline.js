14dcac1427db9d09f4537060d869e888461d2832
Sabrina <swhitney@trinity.edu>
2020-11-06 16:17:35 -0600

    Update interactiveline.js

--- HW5_D3/interactiveline.js
+++ HW5_D3/interactiveline.js
@@ -79,7 +79,7 @@ xAxis2 = g => g
         //renderGraph(data);
         } else {
             const[x0, x1] = selection.map(x.invert);
-            let filterednodes = svg.select("path").filter(
+            let filterednodes = svg.select("path").nodes.filter(
                 function(d) {
                     return d.Date >= x0 && d.Date <= x1;
                 }
