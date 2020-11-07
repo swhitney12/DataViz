e3ad1cbe2aa7914bce38975e1dd3bd22ea631092
Sabrina <swhitney@trinity.edu>
2020-11-06 22:56:16 -0600

    Update interactiveline.js

--- HW5_D3/interactiveline.js
+++ HW5_D3/interactiveline.js
@@ -141,8 +141,8 @@ d3.csv("XOM-XOM.csv")
 // //setting domains
     //x2.domain(d3.extent(data, function(d) { return d.Date; }));
     //y2.domain([0, d3.max(data, function(d) { return d.Close; })]);
-    //x.domain(d3.extent(data, function(d) { return d.Date; }));
-    //y.domain([0, d3.max(data, function(d) { return d.Close; })]);
+    x.domain(d3.extent(data, function(d) { return d.Date; }));
+    y.domain([0, d3.max(data, function(d) { return d.Close; })]);
      x2.domain(x.domain());
      y2.domain(y.domain());
 
@@ -182,15 +182,9 @@ d3.csv("XOM-XOM.csv")
        .call(brush);
 });
 
-function domainupdate(node) {
-    x.domain(d3.extent(node, function(d) { return d.Date; }));
-    y.domain([0, d3.max(node, function(d) { return d.Close; })]);
-}
-
 function renderGraph(nodes) {
     //d3.select("focus > *").remove();
 
-    domainupdate(nodes);
     //can't do domain update in here
   // y.domain([0, d3.max(nodes, function(d) { return d.Close; })]);
     // x.domain(d3.extent(nodes, function(d) { return d.Date; }));
