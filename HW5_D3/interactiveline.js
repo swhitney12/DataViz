09201a35c3278ff3a81f100f6a6a05e2efeb00e0
Sabrina <swhitney@trinity.edu>
2020-11-06 20:24:59 -0600

    Update interactiveline.js

--- HW5_D3/interactiveline.js
+++ HW5_D3/interactiveline.js
@@ -96,8 +96,8 @@ xAxis2 = g => g
             focus.property("Date", selection.map(x2.invert).map(d3.utcDay.round));
             focus.dispatch("input");
             const [x0, x1] = focus;
-            let filterednodes = myData.filter( d => x0 <= d.Date && d.Date <= x1)
-            const maxY = d3.max(filterednodes ? d.Close : NaN);//(myData, d => x0 <= d.Date && d.Date <= x1 ? d.Close : NaN);
+            let filterednodes = myData.filter( d => x0 <= d.Date && d.Date <= x1);
+            const maxY = d3.max(filterednodes ? filterednodes.data.Close : NaN);//(myData, d => x0 <= d.Date && d.Date <= x1 ? d.Close : NaN);
             console.log(`[${x0} , ${x1}]`);
 
             renderGraph(focus, maxY, filterednodes);
