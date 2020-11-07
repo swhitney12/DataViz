4ee8c1676f4265039cbf4ec547157ce89ab782ea
Sabrina <swhitney@trinity.edu>
2020-11-07 00:43:59 -0600

    Update interactiveline.js

--- HW5_D3/interactiveline.js
+++ HW5_D3/interactiveline.js
@@ -168,7 +168,7 @@ function renderGraph(nodes) {
     focus.append("text")
         .attr("transform", "rotate(-90)")
         .attr("y", margin.left)
-        .attr("x", (height /2))
+        .attr("x", 0 - (height /2))
         .attr("dy", "1em")
         .style("text-anchor", "middle")
         .text("Close");
